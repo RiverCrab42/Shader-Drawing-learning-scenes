e627003aba112bf09afbc71efacda8c46ec93217
Vesch <pascalvesch@gmail.com>
2025-07-16 23:59:28 +0700

    Ghost movement

--- intermidiate_project/scripts/ghost.gd
+++ intermidiate_project/scripts/ghost.gd
@@ -1,21 +1,50 @@
 extends Node2D
 
-const SPEED = 40.0
+@export var SPEED = 40.0
 
-var direction = 1
-@onready var right_ray: RayCast2D = $RightRay
-@onready var left_ray: RayCast2D = $LeftRay
-@onready var down_right_ray: RayCast2D = $DownRightRay
-@onready var down_left_ray: RayCast2D = $DownLeftRay
+@onready var animated_sprite_2d: AnimatedSprite2D = $"."
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var area_2d: Area2D = $Area2D
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@export var health : int = 1
+
+var curr_time = 0.0
+var player_enable_time = 2.0
+var is_alive = true;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x += direction * SPEED * delta
-	if (right_ray.is_colliding() or not(down_right_ray.is_colliding())):
-		direction = -1
-		animated_sprite_2d.flip_h = true
-	if (left_ray.is_colliding() or not(down_left_ray.is_colliding())):
-		direction = 1
-		animated_sprite_2d.flip_h = false
+	curr_time += delta
+	if (curr_time > player_enable_time):
+		var direction = (GameManager.player.global_position - global_position).normalized();
+		position += direction * SPEED * delta
+		if (direction.x > 0 and is_alive):
+			animated_sprite_2d.flip_h = true
+		if (direction.x < 0 and is_alive):
+			animated_sprite_2d.flip_h = false
+
+func get_hit() -> void:
+	health -= 1
+	if health == 0:
+		is_alive = false
+		animation_player.play("die")
+		area_2d.collision_mask = 0
+		SPEED = 0.0
+		animation_player.animation_finished.connect(die)
+	else:
+		animation_player.play("hit")
+
+func die(anim_name: StringName) -> void:
+	queue_free()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.get_parent() is Arrow:
+		var arr = area.get_parent() as Arrow
+		if !arr.stuck:
+			get_hit()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.get_hit()
